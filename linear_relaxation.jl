9a315fc7712b6c6c21332787247de6a58cd8e81f
Bruce Rose <brucej.rose@orange.fr>
2023-04-14 16:48:33 +0200

    Started adding the constraints to the model

--- linear_relaxation.jl
+++ linear_relaxation.jl
@@ -1,3 +1,4 @@
+using LinearAlgebra
 using JuMP
 using Cbc
 
@@ -50,7 +51,7 @@ model = Model(Cbc.Optimizer)
 @variable(model, Gl[1:nbStacks, 1:nbItems], lower_bound = 0)
 @variable(model, Gr[1:nbStacks, 1:nbItems], lower_bound = 0)
 
-@variable(model, lambda[[1:nbStacks, 1:nbStacks+1]], lower_bound = 0)
+@variable(model, lambda[[1:nbStacks * (nbStacks+1)/2]], lower_bound = 0)
 
 # MI4 = [[max(TU[:, j]...) for j in 1:first(size(TU[1, :]))] for j in 1:first(size(TI[:, 1]))]
 MI4 = map(Int, (max(TU[:, i]...) for i = 1:first(size(TU[1, :])), j = 1:first(size(TI[:, 1]))))
@@ -127,7 +128,9 @@ function fillXi2!(Xi::BitArray)
         print(m, ":")
         println(n)
         # Xi[m:m+n-1, i] .= 1
-        Xi[m:m+n-1, i:size(Xi)[2]] = identityMat(n)
+        # Xi[m:m+n-1, i:size(Xi)[2]] = diagm([1 for _ in 1:n])
+        # Xi[m:m+n-1, i:size(Xi)[2]] = identityMat(n)
+        Xi[m:m+n-1, i:size(Xi)[2]] = I(n)
         m = m + n
         n = n - 1
         i = i + 1
@@ -135,7 +138,48 @@ function fillXi2!(Xi::BitArray)
     end
 end
 
+function ones(n::Int)
+    return ones(Int8, n, 1)
+end
+
 fillXi1!(Xi1)
 fillXi2!(Xi2)
+
+@constraint(model, cZetaT1, -zetaT >= -ones(size(zetaT)[1]))
+@constraint(model, cZetaT2, -zetaT >= -TIT * ones(size(zetaT)[1]))
+
+@constraint(model, cZetaE1, -zetaT >= -ones(size(zetaT)[1]))
+@constraint(model, cZetaE2, -zetaT >= -TIE * ones(size(zetaT)[1]))
+
+@constraint(model, cTI_F, TI <= F)
+
+@constraint(model, cTI_1_1, transpose(TI) * ones(size(transpose(TI))[1]) <= ones(size(transpose(TI))[1]))
+
+@constraint(model, cTI_TP_IP, transpose(TI) * TP == IP)
+
+@constraint(model, cR_Theta_MI4, R <= Theta * MI4)
+
+@constraint(model, cR_TI_TU, -MI4*(1-Theta) <= R - (transpose(TI) * TU) <= MI4* (1-Theta))
+
+@constraint(model, cR_1_IU, R * ones(size(R)[1]) == IU)
+
+@constraint(model, cTheta_1_1, Theta * ones(size(Theta)[1]) >= ones(size(Theta)[1]))
+
+@constraint(model, cIDE_TI_TDA_IDL, IDE <= transpose(TI) * TDA <= IDL)
+
+@constraint(model, cZ_S_MZ, Z <= S * MZ)
+
+@constraint(model, cPsi_Omega, Psi = transpose(vcat([transpose(Omega[:,i,:]*ones(size(Omega)[1])) for i in 1:size(Omega)[2]]...)))
+
+for j in 1:size(Omega)[2]
+    @constraint(model, cOmega_S_MOmega, Omega[:, j, :] <= S * MOmega)
+end
+for i in 1:size(Omega)[1]
+    @constraint(model, cOmega_TI_S, -(1-S)*MOmega <= Omega[i,:,:] - transpose(TI) <= (1 - S)*MOmega)
+end
+
+@constraint(model, cPsi_ST_MPsi, Psi <= St * MPsi)
+
+# @constraint(model, )
 
 print(model)
