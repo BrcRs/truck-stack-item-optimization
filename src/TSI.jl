9b947b82a5ff7d89094b6cd8605470e93507d84e
Bruce Rose <brucej.rose@orange.fr>
2023-08-18 15:19:59 +0200

    Added include statements for all src files

--- src/TSI.jl
+++ src/TSI.jl
@@ -1,7 +1,17 @@
 module TSI
 
 
+include("column_generation.jl")
+include("instance_loader.jl")
+include("linear_infeasibilities.jl")
+# include("main.jl")
+include("matrix_ops.jl")
 include("model.jl")
-
+include("placement_visualizer.jl")
+include("placement.jl")
+include("progress.jl")
+include("subproblem.jl")
+include("tsiproblem.jl")
+include("uzawa.jl")
 
 end
